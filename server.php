4521abae87ec7c8f9d796b3248d2faeba2151d07
GLVEGA <mcgalv@gmail.com>
2017-05-28 12:38:29 -0500

    sesiones

--- server.php
+++ server.php
@@ -10,18 +10,27 @@ if(isset($_POST["email"]) && isset($_POST["password"]))
 	{
 		// echo "PASS CORRECTA";
 		$_SESSION["login"] = true;
-		$res = array("correcta" => true, "mensaje" => "PASS CORRECTA");
+		$res = array(
+			"correcta" => true, 
+			"mensaje" => "PASS CORRECTA"
+		);
 	}
 	else
 	{
 		// echo "EL PASS NO COINCIDE";
-		$res = array("correcta" => false, "mensaje" => "EL PASS NO COINCIDE");	
+		$res = array(
+			"correcta" => false, 
+			"mensaje" => "EL PASS NO COINCIDE"
+		);	
 	}
 }
 else
 {	
 	// echo "Faltan datos";
-	$res = array("correcta" => false, "mensaje" => "FALTAN DATOS");
+	$res = array(
+		"correcta" => false, 
+		"mensaje" => "FALTAN DATOS"
+	);
 }
 
 echo json_encode($res);
