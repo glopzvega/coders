50393e7c24b70cc79f12d0f620a477427e95264a
GLVEGA <mcgalv@gmail.com>
2017-06-18 13:45:56 -0500

    Publicacion Service Registrar

--- views/publicacionService.php
+++ views/publicacionService.php
@@ -7,8 +7,21 @@ if(isset($_POST["contenido"]))
 	$contenido = $_POST["contenido"];
 	$fecha = date("Y-m-d");
 	$usario = $_SESSION["usuario"];
-	
-	$sql = "INSERT INTO publicacion()"
+
+	$sql = "INSERT INTO publicacion(usuario, fecha, contenido) VALUES ('$usuario', '$fecha', '$contenido')";
+
+	$result = mysqli_query($conn, $sql);
+
+	if($result)
+	{
+		$res = array("success" => true, "mensaje" => "Se ha registrado correctamente");
+		echo json_encode($res);		
+	}
+	else
+	{
+		$res = array("success" => true, "mensaje" => "Ocurrio un error al registrar la publicacion");
+		echo json_encode($res);			
+	}
 }
 else
 {
