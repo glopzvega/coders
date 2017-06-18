f4c0c4be871e46edc2b3bea0dcc0e1dc3a050165
GLVEGA <mcgalv@gmail.com>
2017-06-18 10:10:30 -0500

    Guardar Registro

--- views/registroService.php
+++ views/registroService.php
@@ -2,9 +2,35 @@
 
 if(isset($_GET["first_name"]) && isset($_GET["last_name"]) && isset($_GET["email"]) && isset($_GET["password"]))
 {
-	echo "<pre>";
-	var_dump($_GET);
-	echo "</pre>";
+	// echo "<pre>";
+	// var_dump($_GET);
+	// echo "</pre>";
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "coders";
+	$puerto = "3636";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if (!$conn) {
+	    die("No se pudo conectar: ".mysqli_connect_error());
+	}
+
+	$nombre = $_GET["first_name"];
+	$apellido = $_GET["last_name"];
+	$email = $_GET["email"];
+	$pass = $_GET["password"];
+	$fecha = date("Y-m-d");
+
+	$sql = "INSERT INTO usuarios(nombre, apellido, username, password, fecha) VALUES ('$nombre', '$apellido', '$email', '$pass', '$fecha')";
+
+	$res = mysqli_query($conn, $sql);
+
+	var_dump($res);
+
+
 }
 else
 {
