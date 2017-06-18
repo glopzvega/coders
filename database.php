a901204a87d096127ab1ea12cf2e8d0cff6ad1b8
GLVEGA <mcgalv@gmail.com>
2017-06-18 08:53:24 -0500

    registro.php

--- database.php
+++ database.php
@@ -3,8 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "coders";
+$puerto = "3636";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname, $puerto);
 
 // Check connection
 if (!$conn) {
@@ -14,12 +15,15 @@ echo "Connected successfully";
 
 // $sql = "INSERT INTO usuarios (username, password, fecha) VALUES ('mcgalv@gmail.com', '123456', '". date('Y-m-d')."')";
 
-// $res = mysqli_query($conn, $sql);
-// if ($res) {
-//     echo "New record created successfully" . $res;
-// } else {
-//     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
+
+
+
+$res = mysqli_query($conn, $sql);
+if ($res) {
+    echo "New record created successfully" . $res;
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 mysqli_close($conn);
 ?>
